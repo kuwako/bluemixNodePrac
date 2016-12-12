4dd20af0ea2e1fc79867587803b0142d253d3ae4
kuwako <kuwako@leverages.jp>
2016-12-12 20:18:37 +0900

    楽天から検索できるところまでできた

--- app.js
+++ app.js
@@ -27,6 +27,24 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
 });
 
+var http = require('https');
+var url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222?applicationId=1018675735002256351&keyword=%E3%83%93%E3%83%BC%E3%83%AB&sort=%2reviewAverage&hits=10&page=1';
+http.get(url, function(res) {
+  var body = '';
+  res.setEncoding('utf8');
+
+  res.on('data', function(chunk) {
+      body += chunk;
+  });
+
+  res.on('end', function(res) {
+      ret = JSON.parse(body);
+      console.log(ret);
+  });
+}).on('error', function(e) {
+  console.log(e.message);
+});
+
 var secret = require('./secret.js');
 var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
 var tradeoff_analytics = new TradeoffAnalyticsV1({

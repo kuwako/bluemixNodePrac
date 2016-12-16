2e71de1b266828a29e9839365896a59b8475c8ad
kuwako <kuwako@leverages.jp>
2016-12-16 20:19:32 +0900

    POST受け取るところ1

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
 /*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
+const bodyParser = require('body-parser');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
 var app = express();
-
-// serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
+app.use(bodyParser.urlencode({
+  extended: true
+}));
+app.use(bodyParser.json());
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
 
+app.post('/', function(req, res) {
+  console.log(req.body);
+  console.log(req.body.name);
+});
 var secret = require('./secret.js');
 
 var http = require('https');
 var url = secret.rakuten_url;
+var keyword = "&keyword=" + "ビール";
 
 http.get(url, function(res) {
   var body = '';
@@ -47,7 +39,6 @@ http.get(url, function(res) {
 }).on('error', function(e) {
   console.log(e.message);
 });
-
 var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
 var tradeoff_analytics = new TradeoffAnalyticsV1({
   username: secret.username,
